3b277f0de51a5032153788f5db41d73b58da5af6
Fernando Montealegre <cesarmontealegere@gmail.com>
2024-11-09 06:02:32 -0500

    Update URL

--- mutant_api/app/database.py
+++ mutant_api/app/database.py
@@ -15,7 +15,7 @@ DB_PORT = os.getenv("DB_PORT")  # Database port number
 DB_NAME = os.getenv("DB_NAME")  # Database name
 
 # Form the SQLAlchemy connection URL
-SQLALCHEMY_DATABASE_URL = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl-mode=REQUIRED"
 
 # Create a SQLAlchemy engine for connecting to the database
 engine = create_engine(
